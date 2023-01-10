04c53d292509dd3747b9753d1098a978a3070751
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-01-10 22:23:51 +0800

    Add initial files

---
+++ content_script.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.message === "changed") {
+		let buggedElement = document.querySelector("div[style*='top: -10000px;']");
+		if (buggedElement) {
+			buggedElement.style.display = "none";
+		}
+	}
+});
